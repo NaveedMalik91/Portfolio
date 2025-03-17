a19a69da09b29b23a82f06d9b40368ec4c7ed69c
Naveed Malik <naveedmalik1503@gmail.com>
2025-03-17 15:48:23 +0530

    commit

---
+++ projects/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState === "visible") {
+        document.title = "Projects | Portfolio Jigar Sable";
+        $("#favicon").attr("href", "/assets/images/favicon.png");
+    } else {
+        document.title = "Come Back To Portfolio";
+        $("#favicon").attr("href", "/assets/images/favicon1.png");
+    }
+});
+
+async function getProjects() {
+    try {
+        const response = await fetch("projects.json");
+        const projects = await response.json();
+        showProjects(projects);
+    } catch (error) {
+        console.error("Error loading projects:", error);
+    }
+}
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = projects.map(project => `
+        <div class="grid-item ${project.category}">
+            <div class="box tilt" style="width: 380px; margin: 1rem">
+                <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+                <div class="content">
+                    <div class="tag">
+                        <h3>${project.name}</h3>
+                    </div>
+                    <div class="desc">
+                        <p>${project.desc}</p>
+                        <div class="btns">
+                            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> Use</a>
+                            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    ).join('');
+    
+    projectsContainer.innerHTML = projectsHTML;
+}
+
+getProjects();
